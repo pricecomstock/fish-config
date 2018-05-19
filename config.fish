7c7d8b215c33bb987b75415bb479f106db056fbd
Price Comstock <price.comstock@gmail.com>
2018-05-19 11:35:17 -0500

    added aliases

---
+++ config.fish
@@ -0,0 +1,29 @@
+# Easy i3 editing
+alias i3config='code ~/.config/i3/config'
+
+# Screen layout shortcuts
+alias 25only='~/.screenlayout/25-only.sh'
+alias 2534='~/.screenlayout/34-top-25-bot.sh'
+
+# Random Other Stuff
+alias histgrep='< ~/.bash_history grep'
+alias reload='source ~/.bashrc'
+alias lemmegetdat='sudo apt install -y'
+
+# Shortcuts
+alias messenger='firefox messenger.com'
+alias facheatsheet='chromium-browser http://fontawesome.io/cheatsheet/'
+alias ls='exa'
+
+# SSH
+alias priceserver='ssh 192.168.0.23'
+alias starship='ssh pi@192.168.0.16'
+
+# Project Shortcuts
+alias codepowerrank='code ~/development/powerrankio'
+
+# Try to get monitors correct on login
+# ~/.screenlayout/34-top-25-bot.sh
+
+# Dokku
+alias dokku='ssh -t dokku@159.203.183.58'
